c007fc5d3196910eeada58d9e4d30bab67767a05
RafalSolnica <granadag99@gmail.com>
2023-03-21 23:36:40 +0100

    binding events moved to a seperate function

--- js/main.js
+++ js/main.js
@@ -1,60 +1,96 @@
 {
-    const tasks = [
-        {
-            content: "Skończyć 6 moduł",
-            done: false,
-        },
-        {
-            content: "Iść na 2-godzinny spacer",
-            done: false,
-        },
-        {
-            content: "Zjeść obiad",
-            done: true,
-        },
-    ];
+	const tasks = [
+		{
+			content: "Skończyć 6 moduł",
+			done: false,
+		},
+		{
+			content: "Iść na 2-godzinny spacer",
+			done: true,
+		},
+		{
+			content: "Zjeść obiad",
+			done: true,
+		},
+	];
 
-    const render = () => {
-        let htmlString = "";
+	const addNewTask = (newTaskContent) => {
+		tasks.push({
+			content: newTaskContent,
+		});
 
-        for (const task of tasks) {
-            htmlString += `
+		render();
+	};
+
+	const removeTask = (taskIndex) => {
+		tasks.splice(taskIndex, 1);
+
+		render();
+	};
+
+	const toggleTaskDone = (taskIndex) => {
+		tasks[taskIndex].done = !tasks[taskIndex].done;
+
+		render();
+	};
+
+	const bindEvents = () => {
+		const removeButtons = document.querySelectorAll(".js-remove");
+
+		removeButtons.forEach((removeButton, index) => {
+			removeButton.addEventListener("click", () => {
+				removeTask(index);
+			});
+		});
+
+		const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+		toggleDoneButtons.forEach((toggleDoneButton, index) => {
+			toggleDoneButton.addEventListener("click", () => {
+				toggleTaskDone(index);
+			});
+		});
+	};
+
+	const render = () => {
+		let htmlString = "";
+
+		for (const task of tasks) {
+			htmlString += `
 				<li 
-                    ${task.done ? "style=\"text-decoration: line-through\"" : ""}
-                >
+					${task.done ? 'style="text-decoration: line-through"' : ""}
+				>
+					<button class="js-done">zrobione?</button>
+					<button class="js-remove">usuń</button>
 					${task.content}
 				</li>
-      		`;
-        }
+			`;
+		}
 
-        document.querySelector('.js-tasks').innerHTML = htmlString
-    };
+		document.querySelector(".js-tasks").innerHTML = htmlString;
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        })
+		bindEvents();
+	};
 
-        render()
-    }
+	const onFormSubmit = (e) => {
+		e.preventDefault();
 
-    const onFormSubmit = (e) => {
-        e.preventDefault()
+		const newTaskContent = document
+			.querySelector(".js-newTask")
+			.value.trim();
 
-        const newTaskContent = document.querySelector('.js-newTask').value.trim()
+		if (newTaskContent === "") return;
 
-        if (newTaskContent === '') return
+		addNewTask(newTaskContent);
+	};
 
-        addNewTask(newTaskContent)
-    }
+	const init = () => {
+		render();
 
-    const init = () => {
-        render()
+		const form = document.querySelector(".js-form");
 
-        const form = document.querySelector('.js-form')
+		form.addEventListener("submit", onFormSubmit);
+	};
 
-        form.addEventListener('submit', onFormSubmit)
-    };
-
-    init();
+	init();
 }
